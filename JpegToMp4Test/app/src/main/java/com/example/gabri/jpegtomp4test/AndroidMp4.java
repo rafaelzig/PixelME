2ec8d438f7c489984a72ae4d952c4e60662d9dbe
Gabriel Poulet--de Grimouard <gabriel.degrimouard@gmail.com>
2016-01-04 17:25:14 +0800

    JPegToMp4Test

---
+++ JpegToMp4Test/app/src/main/java/com/example/gabri/jpegtomp4test/AndroidMp4.java
@@ -0,0 +1,146 @@
+package com.example.gabri.jpegtomp4test;
+
+/**
+ * Created by gabri on 12/10/2015.
+ */
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+import org.jcodec.common.NIOUtils;
+import org.jcodec.common.SeekableByteChannel;
+import org.jcodec.common.model.ColorSpace;
+import org.jcodec.common.model.Size;
+import org.jcodec.containers.mp4.Brand;
+import org.jcodec.containers.mp4.MP4Packet;
+import org.jcodec.containers.mp4.TrackType;
+import org.jcodec.containers.mp4.muxer.FramesMP4MuxerTrack;
+import org.jcodec.containers.mp4.muxer.MP4Muxer;
+import org.jcodec.codecs.h264.H264Encoder;
+import org.jcodec.codecs.h264.H264Utils;
+import org.jcodec.scale.BitmapUtil;
+import org.jcodec.scale.ColorUtil;
+import org.jcodec.scale.Transform;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.Yuv420jToRgb;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+
+/**
+ * This class is part of JCodec ( www.jcodec.org ) This software is distributed
+ * under FreeBSD License
+ *
+ * @author The JCodec project
+ *
+ */
+public class AndroidMp4 {
+    private SeekableByteChannel ch;
+    private int frameNo;
+    private MP4Muxer mux;
+    private Size size;
+    private ByteBuffer sps;
+    private ByteBuffer pps;
+    private ArrayList<ByteBuffer> spsList;
+    private ArrayList<ByteBuffer> ppsList;
+    private FramesMP4MuxerTrack outTrack;
+    private ByteBuffer _out;
+    private H264Encoder encoder;
+    private MP4Muxer muxer;
+    private Transform transform;
+    private Picture toEncode;
+
+    public AndroidMp4(File out, int fps) throws IOException {
+        this.ch = NIOUtils.writableFileChannel(out);
+
+        // Muxer that will store the encoded frames
+        muxer = new MP4Muxer(ch, Brand.MP4);
+
+        // Add video track to muxer
+        outTrack = muxer.addTrack(TrackType.VIDEO, fps);
+
+        // Allocate a buffer big enough to hold output frames
+        _out = ByteBuffer.allocate(1920 * 1080 * 2);
+
+        // Create an instance of encoder
+        encoder = new H264Encoder();
+
+        // Transform to convert between RGB and YUV
+        transform = ColorUtil.getTransform(ColorSpace.RGB, encoder.getSupportedColorSpaces()[0]);
+
+        // Encoder extra data ( SPS, PPS ) to be stored in a special place of
+        // MP4
+        spsList = new ArrayList<ByteBuffer>();
+        ppsList = new ArrayList<ByteBuffer>();
+//        this.ch = NIOUtils.writableFileChannel(out);
+//
+//        mux = new MP4Muxer(ch, Brand.MP4);
+//
+//        outTrack = mux.addTrack(TrackType.VIDEO, fps);
+    }
+
+    public void encodeImage(File jpeg) throws IOException {
+        Bitmap read = BitmapFactory.decodeFile(jpeg.getAbsolutePath());
+        Picture pic = BitmapUtil.fromBitmap(read);
+        if (toEncode == null) {
+            toEncode = Picture.create(pic.getWidth(), pic.getHeight(), encoder.getSupportedColorSpaces()[0]);
+        }
+
+
+        // Perform conversion
+        transform.transform(pic, toEncode);
+
+        // Encode image into H.264 frame, the result is stored in '_out' buffer
+        _out.clear();
+        ByteBuffer result = encoder.encodeFrame(toEncode, _out);
+
+        // Based on the frame above form correct MP4 packet
+        spsList.clear();
+        ppsList.clear();
+        H264Utils.wipePS(result, spsList, ppsList);
+//		        NALUnit nu = NALUnit.read(NIOUtils.from(result.duplicate(), FPS));
+        H264Utils.encodeMOVPacket(result);
+
+        // We presume there will be only one SPS/PPS pair for now
+        if (sps == null && spsList.size() != 0)
+            sps = spsList.get(0);
+        if (pps == null && ppsList.size() != 0)
+            pps = ppsList.get(0);
+
+        // Add packet to video track
+        outTrack.addFrame(new MP4Packet(result, frameNo, 1, 1, frameNo, true, null,
+                frameNo, 0));
+
+        frameNo++;
+//        if (size == null) {
+//            Bitmap read = BitmapFactory.decodeFile(jpeg.getAbsolutePath());
+//            size = new Size(read.getWidth(), read.getHeight());
+//        }
+//        // Add packet to video track
+//        outTrack.addFrame(new MP4Packet(NIOUtils.fetchFrom(jpeg), frameNo, 1, 1, frameNo, true, null, frameNo, 0));
+//
+//        frameNo++;
+
+
+    }
+
+    public Bitmap yuv420ToRgb(int width, int height, int[][] data) {
+        Picture pic = new Picture(width, height, data, ColorSpace.YUV420);
+        Picture dst = new Picture(width, height, null, ColorSpace.RGB);
+        Yuv420jToRgb trans = new Yuv420jToRgb();
+        trans.transform(pic, dst);
+        return BitmapUtil.toBitmap(dst);
+    }
+
+    public void finish() throws IOException {
+        outTrack.addSampleEntry(H264Utils.createMOVSampleEntry(spsList, ppsList, 0));
+
+        // Write MP4 header and finalize recording
+        muxer.writeHeader();
+        NIOUtils.closeQuietly(ch);
+        System.out.println("FINISHED !");
+    }
+}
---
+++ JpegToMp4Test/app/src/main/java/com/example/gabri/jpegtomp4test/JpegToMp4.java
@@ -0,0 +1,112 @@
+package com.example.gabri.jpegtomp4test;
+
+import android.os.Environment;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Vector;
+
+/**
+ * Created by gabri on 12/10/2015.
+ */
+public class JpegToMp4 {
+        private File _dir = null;
+        private String              _videoName = null;
+        private Vector<File> _imgFiles;
+        private File        _outDir;
+        /**
+         *
+         * The contructor with the precise images path.
+         *
+         * @param directory the images and output directory.
+         * @param videoName the video name.
+         * @param imagesPath the path of all the pictures.
+         */
+        public JpegToMp4(String directory, String videoName, Vector<String> imagesPath) {
+            _dir = new File(directory);
+            _videoName = videoName;
+            setImageFiles(imagesPath);
+        }
+
+        /**
+         *
+         * The constructor with variable images path.
+         *
+         * @param videoName the output video name.
+         * @param imagesPath the images paths.
+         */
+        public JpegToMp4(String videoName, Vector<String> imagesPath) {
+            _videoName = videoName;
+            setImageFiles(imagesPath);
+        }
+
+        /**
+         *
+         * The contructor with the directory containing only the images.
+         *
+         * @param directory the pictures directory.
+         * @param videoName the video name.
+         */
+        public JpegToMp4(String directory, String videoName) {
+            _dir = new File(directory);
+            _imgFiles = new Vector<>(Arrays.asList(_dir.listFiles()));
+            _videoName = videoName;
+        }
+
+    public JpegToMp4(File outDir, String directory, String videoName) {
+        _dir = new File(directory);
+        _imgFiles = new Vector<>(Arrays.asList(_dir.listFiles()));
+        _videoName = videoName;
+        _outDir = outDir;
+    }
+
+    public void setVideoName(String videoName) {
+            _videoName = videoName;
+        }
+
+        public String getVideoName(){
+            return _videoName;
+        }
+
+        public void setDir(String directory, boolean imgHere) {
+            _dir = new File(directory);
+            if (imgHere) _imgFiles = new Vector<>(Arrays.asList(_dir.listFiles()));
+        }
+
+        public File getDir() {
+            return _dir;
+        }
+
+        public void setImageFiles(Vector<String> imagesPath) {
+            _imgFiles.clear();
+            for (int i = 0; i < imagesPath.size(); i++) {
+                addImage(imagesPath.get(i));
+            }
+        }
+
+        public void addImage(String imgName) {
+            _imgFiles.add(new File((_dir != null ? _dir + File.separator + imgName : imgName)));
+        }
+
+        /**
+         * It will create a mp4 video with the images contained in the directory attribute with the fps send in parameter.
+         * @param fps the frame per second (I advise you to not send less than 6).
+         */
+        public void imagesToVideo(int fps) {
+            String dir = "";
+            if (_dir != null)
+                dir = _dir.getPath();
+            String vName = _videoName;
+            System.out.println(vName);
+            try {
+                File videoFile = new File(_outDir + File.separator + vName);
+                System.out.println(videoFile.toString());
+                AndroidMp4 enc = new AndroidMp4(videoFile, fps);
+                for (File f : _imgFiles)
+                        enc.encodeImage(f);
+                enc.finish();
+            } catch (java.io.IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+}
